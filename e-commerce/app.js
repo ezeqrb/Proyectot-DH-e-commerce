bbfeb594ef4605ea249f8ff25d0c35b749542525
Ezequiel <ezequielromerobertani@gmail.com>
2020-12-04 17:52:20 -0300

    crud

--- e-commerce/app.js
+++ e-commerce/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var shopRouter = require('./routes/shop');
-
+var productRouter = require('./routes/product');
 
 var app = express();
 
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/shop', shopRouter);
+app.use('/product', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
